74294a9470d8cf70a55534a43961b7e7e6f310a2
Nikolas D' Marco José Caixeta <nikolascaixeta19@gmail.com>
2026-09-22 17:33:09 -0300

    Add files via upload

---
+++ Js/attv5.js
@@ -0,0 +1,29 @@
+/*  Crie uma função chamada somarElementos que receba um array de números
+como parâmetro, percorra o vetor, some todos os valores e retorne o total.
+Entrada: array
+Processamento: somar os valores do array
+Saida: a soma
+*/
+function receberArray(){
+  const valores = []
+  let tamanho = Number(prompt("Digite quantos valores que deseja colocar"))
+  for(let i = 0; i < tamanho; i++){
+    valores[i] = Number(prompt(`Digite o ${i+1}º valor`))
+  }
+  return valores
+}
+function somarElementos(vetor){
+    let soma = 0
+    for(let numero of vetor){
+       soma = soma + numero
+        }
+        return soma
+}
+function resultadoSoma(n){
+    alert(`O valor da soma foi ${n}`)
+}
+const array = receberArray()
+
+let resultado = somarElementos(array)
+
+resultadoSoma(resultado)
