7361d41367f1cd0bc3562759e456c9cc422d6e0e
Nikolas D' Marco José Caixeta <nikolascaixeta19@gmail.com>
2026-09-22 16:36:04 -0300

    Add files via upload

---
+++ Js/attv3.js
@@ -0,0 +1,22 @@
+/* . Crie uma função chamada celsiusParaFahrenheit que receba uma temperatura
+em Celsius e retorne a conversão para Fahrenheit (F = (C × 1.8) + 32).
+Entrada: Temperatura em celcius
+Pocessamento: conversão em Fahrenheit
+Saida: Mostrar o valor em Fahrenheit
+*/
+function receberTemp(){
+    let celcius = Number(prompt("Digite a temperatura em °C:"))
+    return celcius
+}
+function celsiusParaFahrenheit(temp){
+    let resultado = (temp * 1.8) + 32
+    return resultado
+}
+function mostrarTemp(Fahrenheit){
+    alert(`A temperatura ${tempC}°C em °F é ${Fahrenheit}`)
+}
+let tempC = receberTemp()
+
+let valor =  celsiusParaFahrenheit(tempC)
+
+mostrarTemp(valor)
